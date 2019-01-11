1e5f5bf6921d0b7f453fd753c482f85788b929ab
liucheng <983045775@qq.com>
2019-01-11 17:49:31 +0800

    99乘法表 demo

--- demo/demo4.py
+++ demo/demo4.py
@@ -1,10 +1,12 @@
-#偶数求和
+# 偶数求和
 i = 0
 count = 0
 
 while i < 100:
     i += 1
     if not (i % 2 == 1):
+        if 90 > i > 10:
+            break
         print("i的值是%d" % i)
         count += i
 
---
+++ demo/demo5.py
@@ -0,0 +1,10 @@
+# 99乘法表
+i = 0
+
+while i < 9:
+    i += 1
+    j = 0
+    while j < i:
+        j += 1
+        print("%dx%d=%d " % (j, i, i * j), end="")
+    print("")
